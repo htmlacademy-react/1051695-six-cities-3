af774f0d472d62478a1529991dbca7056cc7e094
DmitryOO <dimkut@mail.ru>
2026-02-27 20:30:40 +0400

    task 2-1

---
+++ src/components/app.tsx
@@ -0,0 +1,14 @@
+import MainPage from '../pages/main/main';
+
+type MainPageProps = {
+  rentOffersCount: number;
+}
+
+
+function App({rentOffersCount}:MainPageProps): JSX.Element {
+  return (
+    <MainPage rentOffersCount={rentOffersCount}/>
+  );
+}
+
+export default App;
---
+++ src/consts.tsx
@@ -0,0 +1,5 @@
+const Settings = {
+  rentOffersCount:3,
+};
+
+export{Settings};
