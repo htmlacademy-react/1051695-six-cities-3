eb86bf206731cd744d82208b38e21455090f0171
DmitryOO <dimkut@mail.ru>
2026-03-28 14:20:33 +0400

    add Reviews

---
+++ src/components/reviews/reviews.tsx
@@ -0,0 +1,20 @@
+import Review from '../review/review';
+import { commentType } from '../review/review';
+
+
+type reviewsProps = { comments: commentType[] };
+function Reviews({ comments }: reviewsProps) {
+  return (
+    <section>
+      <h2 className="reviews__title">
+        Reviews · <span className="reviews__amount">{comments.length}</span>
+      </h2>
+      {comments.length &&
+        <ul className="reviews__list">
+          {comments.map((comment) => (<Review comment={comment} key={comment.id} />))}
+        </ul>}
+    </section>
+  );
+}
+
+export default Reviews;
--- src/pages/offer-page/offer-page.tsx
+++ src/pages/offer-page/offer-page.tsx
@@ -2,12 +2,12 @@ import Header from '../../components/header/header';
 import OffersList from '../../components/offers-list/offers-list';
 import Map from '../../components/map/map';
 import Form from '../../components/form/form';
+import Reviews from '../../components/reviews/reviews';
 import { mainOfferType } from '../main-page/main-offer-type';
 import { currentOfferType } from './current-offer-type';
-import { commentsType } from './comments-type';
+import { commentType } from '../../components/review/review';
 import { useParams } from 'react-router-dom';
 import NotFoundPage from '../not-found-page/not-found-page';
-import dayjs from 'dayjs';
 import { NEAR_PLACES_MAX_LENGTH } from '../../consts';
 import { CitiesCardClass } from '../../consts';
 
@@ -15,7 +15,7 @@ type offerPageProps = {
   isSignedIn: boolean;
   offers: mainOfferType[];
   currentOffers: currentOfferType[];
-  comments: commentsType;
+  comments: commentType[];
 }
 
 function OfferPage({ isSignedIn, offers, currentOffers, comments }: offerPageProps){
@@ -127,41 +127,7 @@ function OfferPage({ isSignedIn, offers, currentOffers, comments }: offerPagePro
                 </div>
               </div>
               <section className="offer__reviews reviews">
-                <h2 className="reviews__title">
-                  Reviews · <span className="reviews__amount">{comments.length}</span>
-                </h2>
-                <ul className="reviews__list">
-                  {comments.map((comment) => (
-                    <li key={comment.id} className="reviews__item">
-                      <div className="reviews__user user">
-                        <div className="reviews__avatar-wrapper user__avatar-wrapper">
-                          <img
-                            className="reviews__avatar user__avatar"
-                            src={comment.user.avatarUrl}
-                            width={54}
-                            height={54}
-                            alt="Reviews avatar"
-                          />
-                        </div>
-                        <span className="reviews__user-name">{comment.user.name}</span>
-                        {comment.user.isPro && <span className="offer__user-status">Pro</span>}
-                      </div>
-                      <div className="reviews__info">
-                        <div className="reviews__rating rating">
-                          <div className="reviews__stars rating__stars">
-                            <span style={{ width: `${comment.rating * 20}%` }} />
-                            <span className="visually-hidden">Rating</span>
-                          </div>
-                        </div>
-                        <p className="reviews__text">
-                          {comment.comment}
-                        </p>
-                        <time className="reviews__time" dateTime={dayjs(comment.date).format('YYYY-MM-DD')}>
-                          {dayjs(comment.date).format('MMMM YYYY')}
-                        </time>
-                      </div>
-                    </li>))}
-                </ul>
+                <Reviews comments={comments}/>
                 {isSignedIn && <Form />}
               </section>
             </div>
