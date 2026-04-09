6412d0e1c4dd514b802bc589d7950d6e3e5958b4
DmitryOO <dimkut@mail.ru>
2026-04-10 00:31:27 +0400

    fix.

--- src/pages/offer-page/offer-page.tsx
+++ src/pages/offer-page/offer-page.tsx
@@ -117,7 +117,7 @@ function OfferPage({ isSignedIn, offers, comments }: offerPageProps) {
               <div className="offer__host">
                 <h2 className="offer__host-title">Meet the host</h2>
                 <div className="offer__host-user user">
-                  <div className={`offer__avatar-wrapper offer__avatar-wrapper${host.isPro && '--pro'} user__avatar-wrapper`}>
+                  <div className={`offer__avatar-wrapper offer__avatar-wrapper${host.isPro ? '--pro' : ''} user__avatar-wrapper`}>
                     <img
                       className="offer__avatar user__avatar"
                       src={host.avatarUrl}
