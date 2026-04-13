bf0272e9024493e72fc89da9d4eb55c0d6a90d51
DmitryOO <dimkut@mail.ru>
2026-04-13 21:20:53 +0400

    add comments sort

--- src/components/reviews/reviews.tsx
+++ src/components/reviews/reviews.tsx
@@ -1,17 +1,19 @@
 import Review from '../review/review';
 import { commentType } from '../review/review';
+import dayjs from 'dayjs';
 
 
 type reviewsProps = { comments: commentType[] };
 function Reviews({ comments }: reviewsProps) {
+  const sortedSlicedComments = comments.toSorted((comment1,comment2)=>dayjs(comment2.date).diff(dayjs(comment1.date))).slice(0,10);
   return (
     <section>
       <h2 className="reviews__title">
         Reviews · <span className="reviews__amount">{comments.length}</span>
       </h2>
-      {!comments.length ||
+      {!sortedSlicedComments.length ||
         <ul className="reviews__list">
-          {comments.map((comment) => (<Review comment={comment} key={comment.id} />))}
+          {sortedSlicedComments.map((comment) => (<Review comment={comment} key={comment.id} />))}
         </ul>}
     </section>
   );
